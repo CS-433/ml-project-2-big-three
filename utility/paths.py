f6f70de30f5187b1e17084c32ea40669fb93894a
thainamhoang <namhoangthai2607@gmail.com>
2023-12-08 22:20:42 +0100

    add utility and prep

---
+++ utility/paths.py
@@ -0,0 +1,10 @@
+class DataPath:
+    TEST = "data/test_data.csv"
+    TRAIN_NEG_FULL = "data/train_neg_full.txt"
+    TRAIN_POS_FULL = "data/train_pos_full.txt"
+    TRAIN_NEG = "data/train_neg.txt"
+    TRAIN_POS = "data/train_pos.txt"
+
+
+class UtilityPath:
+    SLANG = "utility/files/slang.csv"
